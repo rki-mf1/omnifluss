abcf69cb29f76b6da8cce1b7084ec5cee0e56f6f
Thomas Krannich <Krannich479@users.noreply.github.com>
2025-08-06 14:57:05 +0200

    Add default case to isFileEmpty()

--- lib/FileCheck.groovy
+++ lib/FileCheck.groovy
@@ -2,17 +2,20 @@ import java.util.zip.GZIPInputStream
 
 class FileCheck {
     static boolean isFileEmpty(File file) {
+        // true if the file doesn't exist or is empty (0 bytes)
         if (!file.exists() || file.length() == 0) {
             return true
         }
 
+        // Check if decompressed content is empty
         if (file.name.endsWith('.gz')) {
-            // Check decompressed content
             file.withInputStream { fis ->
                 new GZIPInputStream(fis).withStream { gis ->
                     return gis.read() != -1
                 }
             }
         }
+
+        return false
     }
 }
